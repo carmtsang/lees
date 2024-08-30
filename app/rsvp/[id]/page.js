37f9b2b96f8b919afbd52ef23e2dff679447877d
carmtsang <carmtsang89@hotmail.com>
2024-08-29 20:41:06 -0700

    add remainder pages

--- app/rsvp/[id]/page.js
+++ app/rsvp/[id]/page.js
@@ -1,3 +1,8 @@
-export default function Rsvp() {
-  return <h1>RSVP</h1>;
+export default function Rsvp({ params }) {
+  return (
+    <div>
+      <h1>RSVP</h1>
+      <p>ID: {params.id}</p>
+    </div>
+  );
 }
---
+++ app/weddingparty/page.js
@@ -0,0 +1,8 @@
+export default function WeddingParty() {
+  return (
+    <div>
+      <h1>Wedding Party</h1>
+      <p>pictures of wedding party goes here</p>
+    </div>
+  );
+}
