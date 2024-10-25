92e8a07f61a4870cfdf1da1422c44ed555ac3213
carmtsang <carmtsang89@hotmail.com>
2024-10-24 17:03:31 -0700

    make responsive

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,12 @@
 export default function Header({ font }) {
   return (
     <div className="text-center my-8">
-      <h1 className={`${font} text-8xl`}>Carmen & Rowan</h1>
+      <h1 className={`${font} hidden md:block text-8xl`}>Carmen & Rowan</h1>
+      <h1 className={`${font} block md:hidden text-7xl`}>
+        Carmen
+        <br /> &<br />
+        Rowan
+      </h1>
     </div>
   );
 }
