9e54c3c2566a33d77b312145c0304ce0f47ace82
carmtsang <carmtsang89@hotmail.com>
2024-10-24 17:03:05 -0700

    add pics

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-        <div className="min-h-screen max-w-5xl mx-auto p-4 bg-white">
+        <div className="p5 max-w-6xl mx-auto bg-white min-h-screen">
           <Header font={allura.className} />
           <Navbar />
           {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
     colors: {
       green: "#516a50",
       pink: "#eeaba9",
