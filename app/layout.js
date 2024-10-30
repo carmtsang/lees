01db400c9f273772f33a89125f80762f2f3de0f5
carmtsang <carmtsang89@hotmail.com>
2024-10-29 19:13:39 -0700

    add colours

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-        <div className="p5 max-w-6xl mx-auto bg-white min-h-screen">
+        <div className="max-w-6xl mx-auto min-h-screen my-9 p-5">
           <Header font={allura.className} />
           <Navbar />
           {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,13 +9,26 @@ module.exports = {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
     colors: {
+      transparent: "transparent",
       green: "#516a50",
       pink: "#eeaba9",
       gray: "#888a7d",
       red: "#7a0b13",
       "green-light": "#9cb2a5",
+      black: colors.black,
+      white: colors.white,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      purple: colors.purple,
     },
   },
 
